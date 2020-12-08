c4f7c2f21ed0f7773863be66578e0e58c1ab266f
TEOTD <kiransrivani@gmail.com>
2020-12-08 13:32:55 +0530

    4th commit

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -1,7 +1,46 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Dicee(),
       ),
     );
+
+class Dicee extends StatefulWidget {
+  @override
+  _DiceeState createState() => _DiceeState();
+}
+
+class _DiceeState extends State<Dicee> {
+  int ballnum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        appBar: AppBar(
+          title: Center(child: Text('Ask Me Anything')),
+          backgroundColor: Colors.indigo,
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      ballnum = Random().nextInt(5) + 1;
+                    });
+                  },
+                  child: Image.asset('images/ball$ballnum.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
